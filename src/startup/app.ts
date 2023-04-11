4e29a88ffdff3925c600d40ac8769ad715742343
chamathr <chamatht20@gmailcom>
2023-04-11 10:01:19 +0530

    changes

--- src/startup/app.ts
+++ src/startup/app.ts
@@ -5,11 +5,23 @@ import { errors } from '../middlewares/errors';
 import helmet from "helmet"
 import cookieParser from 'cookie-parser'
 import logger from 'morgan'
+import swaggerUi from 'swagger-ui-express'
+import swaggerJSDoc from 'swagger-jsdoc'
 
 export default (app: Application) => {
     app.use(helmet());
     app.use(logger('dev'));
     app.use(express.json());
+
+    /*Swagger configuration*/
+    const swaggerOptions = {
+        definition: require('../../swagger.json'),
+        apis: ['../routes/*.js'],
+    };
+    const swaggerSpec = swaggerJSDoc(swaggerOptions);
+    /*Swagger UI*/
+    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
     app.use(cors());
     app.use(express.urlencoded());
     app.use(cookieParser());
